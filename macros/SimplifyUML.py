a9f0e36fc11625491c27e257f776fdd1ae2b6882
picotg <picotg@f217-05.e-im2ag.ujf-grenoble.fr>
2015-03-24 08:53:36 +0100

    to GenORM

---
+++ macros/SimplifyUML.py
@@ -0,0 +1,29 @@
+"""
+=========================================================
+                       SimplifyUML.py
+ Generate a USE OCL specification from a UML package
+=========================================================
+
+FILL THIS SECTION AS SHOWN BELOW AND LINES STARTING WITH ###
+@author Joris ROSIER <rosier.joris@hotmail.fr>
+@author Gael PICOT <gael.picot@free.fr>
+@group  G236
+
+Current state of the generator
+----------------------------------
+FILL THIS SECTION 
+Explain which UML constructs are supported, which ones are not.
+What is good in your generator?
+What are the current limitations?
+
+Current state of the tests
+--------------------------
+FILL THIS SECTION 
+Explain how did you test this generator.
+Which test are working? 
+Which are not?
+
+Observations
+------------
+Additional observations could go there
+"""
